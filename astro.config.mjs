5b93a9e825f974e7383500265e810cefe1e27ae9
NicholasNaufal <nicholas.naufal@hotmail.com>
2026-03-02 00:07:16 -0300

    fix(site): specific course links, search logic and analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://edubot-pro.vercel.app',
-    integrations: [tailwind()],
+    integrations: [tailwind(), react()],
     output: 'static',
     compressHTML: true,
     build: {
